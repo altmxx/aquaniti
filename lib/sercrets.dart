60096923ef530db951ca12e255134eb5b5141069
altmxx <altamashalam19@gmail.com>
2023-09-25 14:09:54 +0530

    Refresh gitignore

--- lib/sercrets.dart
+++
@@ -1,7 +0,0 @@
-class Secrets {
-  static const String XMasterKey =
-      "\$2a\$10\$37WMcDhBmhP.FyahXh5uQu2AyWocAN0OFiUKz.ScOHzNCUZLtSrJq";
-
-  static const String XAccessKey =
-      "\$2a\$10\$nYrT1g.hJtG1Z34aOKNwN.7z0pMBXMoqqZplN5o.LeLqgLWWdyTem";
-}
