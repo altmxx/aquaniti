66d8f361bc2ea9e53c01eb9c1dcf1259ae62bbcc
altmxx <altamashalam19@gmail.com>
2023-11-27 22:02:44 +0530

    Refactor home screen layout

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -31,57 +31,74 @@ class _HomeScreenState extends State<HomeScreen> {
     //   homeProvider.getArticles();
     // }
     return Scaffold(
-        body: Center(
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            verticalSpace(30.h),
-            Container(
-              width: 314.w,
-              height: 180.h,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(10.r),
-              ),
-            ),
-            verticalSpace(26.h),
-            Container(
-              margin: EdgeInsets.only(left: 23.w),
-              width: double.infinity,
-              alignment: Alignment.centerLeft,
-              child: Text(
-                AppLocalizations.of(context)!.popularArticles,
-                style: TextStyle(
-                  fontSize: 14.sp,
-                  fontWeight: FontWeight.w400,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              verticalSpace(30.h),
+              Container(
+                width: 314.w,
+                height: 180.h,
+                decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10.r),
                 ),
               ),
-            ),
-            // ArticleWidget()
-            // if (homeProvider.article.isNotEmpty)
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 23.w),
-              child: GridView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 20.w,
-                  mainAxisSpacing: 12.h,
-                  childAspectRatio: 142.w / 200.h,
+              verticalSpace(26.h),
+              Container(
+                margin: EdgeInsets.only(left: 23.w),
+                width: double.infinity,
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  AppLocalizations.of(context)!.popularArticles,
+                  style: TextStyle(
+                    fontSize: 14.sp,
+                    fontWeight: FontWeight.w400,
+                  ),
                 ),
-                itemBuilder: (ctx, i) {
-                  // homeProvider.getArticles();
-                  return ArticleWidget(article: homeProvider.article[i]);
-                },
-                itemCount: homeProvider.article.length,
               ),
-            ),
-            verticalSpace(24.h),
-          ],
+              // ArticleWidget()
+              // if (homeProvider.article.isNotEmpty)
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 23.w),
+                child: GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 20.w,
+                    mainAxisSpacing: 12.h,
+                    childAspectRatio: 142.w / 200.h,
+                  ),
+                  itemBuilder: (ctx, i) {
+                    // homeProvider.getArticles();
+                    return ArticleWidget(article: homeProvider.article[i]);
+                  },
+                  itemCount: homeProvider.article.length,
+                ),
+              ),
+              verticalSpace(24.h),
+            ],
+          ),
         ),
       ),
-    ));
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {},
+          label: const Row(
+            children: [
+              Text(
+                "Marketplace",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 0, 115, 209),
+                ),
+              ),
+              Icon(
+                Icons.shopping_bag_outlined,
+                color: Color.fromARGB(255, 0, 115, 209),
+              )
+            ],
+          )),
+    );
   }
 }
